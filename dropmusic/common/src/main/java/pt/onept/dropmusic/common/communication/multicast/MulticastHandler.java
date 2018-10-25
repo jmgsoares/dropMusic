ac8a33feed7a9cf3641548575a5f059d5a63d298
João Soares <joaomgsoares@gmail.com>
2018-10-25 03:25:23 +0100

    WIP - Multicast / Protocol ❌ - no good

--- dropmusic/common/src/main/java/pt/onept/dropmusic/common/communication/multicast/MulticastHandler.java
+++ dropmusic/common/src/main/java/pt/onept/dropmusic/common/communication/multicast/MulticastHandler.java
@@ -1,23 +1,31 @@
 package pt.onept.dropmusic.common.communication.multicast;
 
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.MulticastSocket;
 import java.util.UUID;
+import java.util.concurrent.BlockingQueue;
 
-public class MulticastHandler extends Thread{
+public class MulticastHandler {
 	private String address;
 	private int port;
 	private UUID uuid;
+	private BlockingQueue<String> messageQueue;
+
 
 	public MulticastHandler(String address, int port, UUID uuid) {
 		this.address = address;
 		this.port = port;
 		this.uuid = uuid;
-		ReceiveDatagram receive = new ReceiveDatagram(this.address, this.port, uuid);
-		receive.start();
 	}
 
-	public void send(byte[] buffer) {
-		SendDatagram send;
-		send = new SendDatagram(this.address, this.port, buffer, this.uuid);
-		send.start();
+	public receive(String address, int port, UUID uuid){
+		try {
+			MulticastSocket socket = new MulticastSocket(port);
+			InetAddress group = InetAddress.getByName(address);
+			socket.joinGroup(group);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 }
--- dropmusic/server/src/main/java/pt/onept/dropmusic/server/service/ArtistManager.java
+++ dropmusic/server/src/main/java/pt/onept/dropmusic/server/service/ArtistManager.java
@@ -24,6 +24,9 @@ public class ArtistManager extends UnicastRemoteObject implements ArtistManagerI
 	public Artist read(Long id) throws NotFoundException, UnauthorizedException, RemoteException {
 		Artist artist = new Artist(1,"TEST ARTIST");
 
+
+
+
 		return artist;
 	}
 
