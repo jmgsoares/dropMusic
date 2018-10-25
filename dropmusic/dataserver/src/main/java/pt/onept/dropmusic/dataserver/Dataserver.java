fb54f6170ee2ceace8e89a6e8fcd319bb2e2487a
João Soares <joaomgsoares@gmail.com>
2018-10-26 00:26:32 +0100

    changed the DBMS to postgres 👌👌👌

--- dropmusic/dataserver/src/main/java/pt/onept/dropmusic/dataserver/Dataserver.java
+++ dropmusic/dataserver/src/main/java/pt/onept/dropmusic/dataserver/Dataserver.java
@@ -2,15 +2,14 @@ package pt.onept.dropmusic.dataserver;
 
 import pt.onept.dropmusic.dataserver.database.DatabaseHandler;
 
-import java.io.IOException;
-
 public class Dataserver {
 
-	public static void main(String[] args) throws IOException {
-		DatabaseHandler db;
-		db = new DatabaseHandler();
-		db.getDBConnection();
-		db.createDB();
-
+	public static void main(String[] args) {
+		DatabaseHandler dataBase;
+		String url = "jdbc:postgresql://onept.pt:65112/dropmusic";
+		String dbUser = "dropmusicDBuser";
+		String dbUserPassword = "dropmusicDBpassword";
+		dataBase = new DatabaseHandler(url,dbUser,dbUserPassword);
+		dataBase.executeSqlStatement(dataBase.getSqlScriptFromFile("recreate_dropmusic_sql.postgres.sql"));
 	}
 }
