30f86ba759cf3bb5a349c5090e12b54364079cef
João Soares <joaomgsoares@gmail.com>
2018-10-25 20:52:55 +0100

    no DBgit add -A

--- dropmusic/dataserver/src/main/java/pt/onept/dropmusic/dataserver/Dataserver.java
+++ dropmusic/dataserver/src/main/java/pt/onept/dropmusic/dataserver/Dataserver.java
@@ -4,19 +4,13 @@ import pt.onept.dropmusic.dataserver.database.DatabaseHandler;
 
 import java.io.IOException;
 
-import java.sql.SQLException;
-
-
 public class Dataserver {
 
-    public static void main(String[] args) throws IOException {
-        DatabaseHandler db;
-        db = new DatabaseHandler();
-        try {
-            db.getDBConnection();
-            db.createDB();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
+	public static void main(String[] args) throws IOException {
+		DatabaseHandler db;
+		db = new DatabaseHandler();
+		db.getDBConnection();
+		db.createDB();
+
+	}
 }
--- dropmusic/dataserver/src/main/java/pt/onept/dropmusic/dataserver/database/DatabaseHandler.java
+++ dropmusic/dataserver/src/main/java/pt/onept/dropmusic/dataserver/database/DatabaseHandler.java
@@ -2,12 +2,16 @@ package pt.onept.dropmusic.dataserver.database;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.stream.Collectors;
+
+import oracle.jdbc.pool.OracleConnectionPoolDataSource;
 import oracle.jdbc.pool.OracleDataSource;
 
 public class DatabaseHandler {
@@ -15,23 +19,64 @@ public class DatabaseHandler {
 
     public DatabaseHandler() {}
 
-    public void getDBConnection () throws SQLException {
-        String dbUrl = "jdbc:oracle:thin:@onept.pt:1521:XE";
-        OracleDataSource ds = new OracleDataSource();
-        ds.setURL(dbUrl);
-        db = ds.getConnection("bd","bd");
+    public void getDBConnection () {
+        String dbUrl = "jdbc:oracle:thin:bd/bd@onept.pt:65112:XE";
+	    try {
+		    OracleDataSource ds = new OracleConnectionPoolDataSource();
+		    ds.setURL(dbUrl);
+		    ds.setDatabaseName("BD");
+		    System.out.println(ds.toString());
+		    this.db = ds.getConnection();
+	    } catch (SQLException e) {
+		    e.printStackTrace();
+	    }
+
     }
 
-    private String getSqlScript () throws IOException {
-        ClassLoader classLoader = getClass().getClassLoader();
-        File file = new File(classLoader.getResource("recreate_dropmusic_db.sql").getFile());
-        byte[] bin = Files.readAllBytes(Paths.get(file.getPath()));
-        return new String(bin);
-    }
+//    private String getSqlScript () throws NullPointerException{
+//        ClassLoader classLoader = getClass().getClassLoader();
+//	    URL resource = classLoader.getResource("new_dropmusic_db.sql");
+//	    if(resource == null) throw new NullPointerException();
+//        File file = new File(resource.getFile());
+//	    byte[] bin = new byte[0];
+//	    try {
+//		    bin = Files.readAllBytes(Paths.get(file.getPath()));
+//	    } catch (IOException e) {
+//		    e.printStackTrace();
+//		    System.out.println("Could not read sql file to create the database");
+//	    }
+//	    return new String(bin);
+//    }
 
-    public void createDB() throws SQLException, IOException {
-        Statement statement = this.db.createStatement();
-        statement.executeUpdate(getSqlScript());
+	private String getSqlScript () throws NullPointerException{
+		ClassLoader classLoader = getClass().getClassLoader();
+		URL resource = classLoader.getResource("new_dropmusic_db.sql");
+		if(resource == null) throw new NullPointerException();
+		File cs = new File(resource.getPath());
+		String sql;
+		try {
+			sql =  Files.lines(Paths.get(resource.getPath())).collect(Collectors.joining("\n"));
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.out.println("Could not read sql file to create the database");
+			throw new NullPointerException();
+		}
+		return sql;
+	}
+
+
+	public void createDB() {
+	    try {
+		    Statement statement = this.db.createStatement();
+		    String data = getSqlScript();
+
+		    statement.execute(data);
+		    System.out.println(statement.getConnection().isValid(1000));
+	    } catch (SQLException e) {
+		    e.printStackTrace();
+		    System.out.println("Could not create the database");
+	    }
+
     }
 
     public boolean executeSql(String sql) throws SQLException {
