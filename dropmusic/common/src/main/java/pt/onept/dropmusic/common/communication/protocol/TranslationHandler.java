1b61c86f27fa6cdbc08781050f588f62668e8948
João Soares <joaomgsoares@gmail.com>
2018-10-26 02:01:44 +0100

    WIP - TranslationHandler class for protocol

---
+++ dropmusic/common/src/main/java/pt/onept/dropmusic/common/communication/protocol/TranslationHandler.java
@@ -0,0 +1,19 @@
+package pt.onept.dropmusic.common.communication.protocol;
+
+import com.google.gson.Gson;
+
+public final class TranslationHandler {
+
+	private TranslationHandler() {}
+
+	public Gson messageToGson() {
+		return null;
+	}
+
+	public Gson fromGson() {
+		return null;
+	}
+
+
+
+}
