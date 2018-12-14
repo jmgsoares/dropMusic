d7f7440b3cfa8d599c2c2235f3c6ac851a484ce1
João Soares <joaomgsoares@gmail.com>
2018-12-14 17:53:33 +0000

    refactored user actions to one class

--- dropmusic/web/src/main/java/pt/onept/sd1819/dropmusic/web/action/UserAction.java
+++ dropmusic/web/src/main/java/pt/onept/sd1819/dropmusic/web/action/UserAction.java
@@ -5,6 +5,8 @@ import com.opensymphony.xwork2.ActionSupport;
 import com.opensymphony.xwork2.ModelDriven;
 import org.apache.struts2.interceptor.SessionAware;
 import pt.onept.sd1819.dropmusic.common.exception.DataServerException;
+import pt.onept.sd1819.dropmusic.common.exception.DuplicatedException;
+import pt.onept.sd1819.dropmusic.common.exception.IncompleteException;
 import pt.onept.sd1819.dropmusic.common.exception.UnauthorizedException;
 import pt.onept.sd1819.dropmusic.common.server.contract.subcontract.UserManagerInterface;
 import pt.onept.sd1819.dropmusic.common.server.contract.type.User;
@@ -13,11 +15,11 @@ import pt.onept.sd1819.dropmusic.web.communication.CommunicationManager;
 import java.rmi.RemoteException;
 import java.util.Map;
 
-public class LoginAction extends ActionSupport implements SessionAware, ModelDriven<User> {
+public class UserAction extends ActionSupport implements SessionAware, ModelDriven<User> {
 	private User user = new User();
 	private Map<String, Object> session;
 
-	public String execute() throws Exception {
+	public String login() throws Exception {
 		try {
 			UserManagerInterface userManager = CommunicationManager.getServerInterface().user();
 			this.user = userManager.login(this.user);
@@ -36,6 +38,27 @@ public class LoginAction extends ActionSupport implements SessionAware, ModelDri
 		return Action.ERROR;
 	}
 
+	public String logout() throws Exception {
+		this.session.clear();
+		return Action.SUCCESS;
+	}
+
+	public String register() throws Exception {
+		try {
+			UserManagerInterface userManager = CommunicationManager.getServerInterface().user();
+			userManager.create(user, null);
+			addActionMessage("User " + user.getUsername() + " created successfully");
+			return Action.SUCCESS;
+		} catch (DuplicatedException e) {
+			addActionError("User " + user.getUsername() + " already exists");
+		} catch (IncompleteException e) {
+			addActionError("Some data fields were left empty");
+		} catch (RemoteException | UnauthorizedException | DataServerException e) {
+			e.printStackTrace();
+			addActionError("There was an error around here");
+		}
+		return Action.ERROR;
+	}
 
 	public User getModel() {
 		return this.user;
