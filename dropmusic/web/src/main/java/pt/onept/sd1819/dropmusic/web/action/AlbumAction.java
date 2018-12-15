244289592695b00364d2ef9b3d2d81aeed985e40
João Soares <joaomgsoares@gmail.com>
2018-12-15 01:20:13 +0000

    updated actions to add list functions

--- dropmusic/web/src/main/java/pt/onept/sd1819/dropmusic/web/action/AlbumAction.java
+++ dropmusic/web/src/main/java/pt/onept/sd1819/dropmusic/web/action/AlbumAction.java
@@ -3,29 +3,34 @@ package pt.onept.sd1819.dropmusic.web.action;
 import com.opensymphony.xwork2.Action;
 import com.opensymphony.xwork2.ActionSupport;
 import com.opensymphony.xwork2.ModelDriven;
-import org.apache.struts2.interceptor.SessionAware;
 import pt.onept.sd1819.dropmusic.common.exception.DataServerException;
 import pt.onept.sd1819.dropmusic.common.exception.DuplicatedException;
 import pt.onept.sd1819.dropmusic.common.exception.IncompleteException;
 import pt.onept.sd1819.dropmusic.common.exception.UnauthorizedException;
 import pt.onept.sd1819.dropmusic.common.server.contract.subcontract.AlbumManagerInterface;
 import pt.onept.sd1819.dropmusic.common.server.contract.subcontract.ArtistManagerInterface;
+import pt.onept.sd1819.dropmusic.common.server.contract.subcontract.MusicManagerInterface;
 import pt.onept.sd1819.dropmusic.common.server.contract.type.Album;
-import pt.onept.sd1819.dropmusic.common.server.contract.type.User;
+import pt.onept.sd1819.dropmusic.common.server.contract.type.Artist;
+import pt.onept.sd1819.dropmusic.web.LoginAware;
 import pt.onept.sd1819.dropmusic.web.communication.CommunicationManager;
 
 import java.rmi.RemoteException;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
 
-public class AlbumAction extends ActionSupport implements SessionAware, ModelDriven<Album> {
+public class AlbumAction extends ActionSupport implements LoginAware, ModelDriven<Album> {
 	private Album album = new Album();
 	private Map<String, Object> session;
+	private List<Artist> artistList;
 
 	public String create() throws Exception {
 		if (album.getName()==null) return Action.INPUT;
 		try {
 			AlbumManagerInterface albumManager = CommunicationManager.getServerInterface().album();
-			albumManager.create((User)session.get("user"), album);
+			albumManager.create(this.getUser(), album);
 			addActionMessage("Album created successfully");
 			return Action.SUCCESS;
 		} catch (DuplicatedException e) {
@@ -40,7 +45,8 @@ public class AlbumAction extends ActionSupport implements SessionAware, ModelDri
 		} catch (UnauthorizedException e) {
 			addActionError("You lack the permissions to perform the operation");
 			return Action.ERROR;
-		}	}
+		}
+	}
 
 	public String read() throws Exception {
 		return Action.SUCCESS;
@@ -54,6 +60,16 @@ public class AlbumAction extends ActionSupport implements SessionAware, ModelDri
 		return Action.SUCCESS;
 	}
 
+	public List<Artist> getArtistList() {
+		try {
+			ArtistManagerInterface artistManager = CommunicationManager.getServerInterface().artist();
+			return artistManager.list(this.getUser());
+		} catch (DataServerException | RemoteException e) {
+			e.printStackTrace();
+		}
+		return new LinkedList<>();
+	}
+
 	@Override
 	public Album getModel() {
 		return this.album;
@@ -62,5 +78,10 @@ public class AlbumAction extends ActionSupport implements SessionAware, ModelDri
 	@Override
 	public void setSession(Map<String, Object> map) {
 		this.session = map;
+	}
+
+	@Override
+	public Map<String, Object> getSession() {
+		return this.session;
 	}
 }
--- dropmusic/web/src/main/java/pt/onept/sd1819/dropmusic/web/action/SearchAction.java
+++ dropmusic/web/src/main/java/pt/onept/sd1819/dropmusic/web/action/SearchAction.java
@@ -2,17 +2,16 @@ package pt.onept.sd1819.dropmusic.web.action;
 
 import com.opensymphony.xwork2.Action;
 import com.opensymphony.xwork2.ActionSupport;
-import org.apache.struts2.interceptor.SessionAware;
 import pt.onept.sd1819.dropmusic.common.exception.DataServerException;
 import pt.onept.sd1819.dropmusic.common.server.contract.type.Album;
-import pt.onept.sd1819.dropmusic.common.server.contract.type.User;
+import pt.onept.sd1819.dropmusic.web.LoginAware;
 import pt.onept.sd1819.dropmusic.web.communication.CommunicationManager;
 
 import java.rmi.RemoteException;
 import java.util.List;
 import java.util.Map;
 
-public class SearchAction extends ActionSupport implements SessionAware {
+public class SearchAction extends ActionSupport implements LoginAware {
 	private String query = "";
 	Map<String, Object> session;
 	List<Album> albums;
@@ -21,7 +20,7 @@ public class SearchAction extends ActionSupport implements SessionAware {
 	public String execute() throws Exception {
 		if (query == null || query.isEmpty()) return Action.INPUT;
 		try {
-			this.albums = CommunicationManager.getServerInterface().album().search((User) this.session.get("user"), query);
+			this.albums = CommunicationManager.getServerInterface().album().search(this.getUser(), query);
 			return Action.SUCCESS;
 		} catch (RemoteException | DataServerException e) {
 			e.printStackTrace();
@@ -46,5 +45,10 @@ public class SearchAction extends ActionSupport implements SessionAware {
 
 	public String getQuery() {
 		return query;
+	}
+
+	@Override
+	public Map<String, Object> getSession() {
+		return this.session;
 	}
 }
