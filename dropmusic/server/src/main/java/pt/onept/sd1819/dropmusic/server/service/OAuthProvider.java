f3cae26dcd9de77b3dc7b77ad9f3d4d228646c92
João Soares <joaomgsoares@gmail.com>
2018-12-16 21:49:25 +0000

    Refactored name of OAuthServiceProvider

--- dropmusic/server/src/main/java/pt/onept/sd1819/dropmusic/server/service/OAuthProvider.java
+++ dropmusic/server/src/main/java/pt/onept/sd1819/dropmusic/server/service/OAuthProvider.java
@@ -1,14 +1,14 @@
 package pt.onept.sd1819.dropmusic.server.service;
 
-import pt.onept.sd1819.dropmusic.common.server.contract.subcontract.DropBoxRestManagerInterface;
+import pt.onept.sd1819.dropmusic.common.server.contract.subcontract.OAuthProviderInterface;
 import pt.onept.sd1819.dropmusic.server.service.rest.DropBox20;
 
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
 
-public class DropBoxManager extends UnicastRemoteObject implements DropBoxRestManagerInterface {
+public class OAuthProvider extends UnicastRemoteObject implements OAuthProviderInterface {
 
-	public DropBoxManager() throws RemoteException {
+	public OAuthProvider() throws RemoteException {
 		super();
 	}
 
