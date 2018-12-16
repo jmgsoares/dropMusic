d7a4a96981eea3e7580ef1d55bb491b3fc9a28ee
João Soares <joaomgsoares@gmail.com>
2018-12-16 04:03:48 +0000

    renamed file | inserted global script file

--- dropmusic/web/src/main/webapp/template/scripts/toggleDiv.js
+++
@@ -1,8 +0,0 @@
-function toggleDiv() {
-    let x = document.getElementById("toggleableDiv");
-    if (x.style.display === "none") {
-        x.style.display = "block";
-    } else {
-        x.style.display = "none";
-    }
-}
