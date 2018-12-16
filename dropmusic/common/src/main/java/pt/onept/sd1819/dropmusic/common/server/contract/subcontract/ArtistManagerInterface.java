264b949d3774cdca06a1fb32607b5efb5afc1617
João Soares <joaomgsoares@gmail.com>
2018-12-16 21:48:57 +0000

    Added function to clean artists

--- dropmusic/common/src/main/java/pt/onept/sd1819/dropmusic/common/server/contract/subcontract/ArtistManagerInterface.java
+++ dropmusic/common/src/main/java/pt/onept/sd1819/dropmusic/common/server/contract/subcontract/ArtistManagerInterface.java
@@ -1,11 +1,14 @@
 package pt.onept.sd1819.dropmusic.common.server.contract.subcontract;
 
+import pt.onept.sd1819.dropmusic.common.exception.DataServerException;
 import pt.onept.sd1819.dropmusic.common.server.contract.Crudable;
 import pt.onept.sd1819.dropmusic.common.server.contract.Listable;
 import pt.onept.sd1819.dropmusic.common.server.contract.type.Artist;
+import pt.onept.sd1819.dropmusic.common.server.contract.type.User;
 
 import java.io.Serializable;
 import java.rmi.Remote;
+import java.rmi.RemoteException;
 
 /**
  * Manage platform Albums
@@ -14,4 +17,6 @@ import java.rmi.Remote;
  * @version 1e-1024
  */
 public interface ArtistManagerInterface extends Remote, Crudable<Artist>, Listable<Artist>, Serializable {
+
+	void clean(User self) throws RemoteException, DataServerException;
 }
