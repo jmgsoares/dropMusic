9447ec019cb800e428590fa90d199a014941acb6
João Soares <joaomgsoares@gmail.com>
2018-12-16 19:15:29 +0000

    added remote RMI interface to DropBox Rest service

---
+++ dropmusic/common/src/main/java/pt/onept/sd1819/dropmusic/common/server/contract/subcontract/DropBoxRestManagerInterface.java
@@ -0,0 +1,14 @@
+package pt.onept.sd1819.dropmusic.common.server.contract.subcontract;
+
+import java.io.Serializable;
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+public interface DropBoxRestManagerInterface extends Remote, Serializable {
+
+	Boolean getLocal() throws RemoteException;
+
+	String getAccessTokenResponse(String authorizationCode) throws RemoteException;
+
+	String getAuthorizationUrl() throws RemoteException;
+}
