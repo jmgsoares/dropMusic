2b6291c919784d2d6ee151695d773ff3206d2b7c
João Soares <joaomgsoares@gmail.com>
2018-12-17 01:46:54 +0000

    review scores on albums get updated on each review

--- dropmusic/dataserver/src/main/resources/createDB.sql
+++ dropmusic/dataserver/src/main/resources/createDB.sql
@@ -75,333 +75,333 @@ drop sequence if exists s_ACCOUNT;
 /* Table: ALBUM                                                 */
 /*==============================================================*/
 create table ALBUM (
-                     ID          SERIAL        not null,
-                     NAME        VARCHAR(64)   not null,
-                     DESCRIPTION VARCHAR(1024) not null,
-                     SCORE       FLOAT8        null,
-                     UNIQUE (NAME, DESCRIPTION),
-                     constraint PK_ALBUM primary key (ID)
+	                   ID          SERIAL        not null,
+	                   NAME        VARCHAR(64)   not null,
+	                   DESCRIPTION VARCHAR(1024) not null,
+	                   SCORE       FLOAT8        null,
+	                   UNIQUE (NAME, DESCRIPTION),
+	                   constraint PK_ALBUM primary key (ID)
 );
 
 /*==============================================================*/
 /* Index: ALBUM_PK                                              */
 /*==============================================================*/
 create unique index ALBUM_PK
-  on ALBUM (
-            ID
-    );
+	on ALBUM (
+	          ID
+		);
 
 /*==============================================================*/
 /* Table: ARTIST                                                */
 /*==============================================================*/
 create table ARTIST (
-                      ID   SERIAL             not null,
-                      NAME VARCHAR(32) unique not null,
-                      constraint PK_ARTIST primary key (ID)
+	                    ID   SERIAL             not null,
+	                    NAME VARCHAR(32) unique not null,
+	                    constraint PK_ARTIST primary key (ID)
 );
 
 /*==============================================================*/
 /* Index: ARTIST_PK                                             */
 /*==============================================================*/
 create unique index ARTIST_PK
-  on ARTIST (
-             ID
-    );
+	on ARTIST (
+	           ID
+		);
 
 /*==============================================================*/
 /* Table: ARTIST_ALBUM                                          */
 /*==============================================================*/
 create table ARTIST_ALBUM (
-                            ID     INT4 not null,
-                            ALB_ID INT4 not null,
-                            constraint PK_ARTIST_ALBUM primary key (ID, ALB_ID)
+	                          ID     INT4 not null,
+	                          ALB_ID INT4 not null,
+	                          constraint PK_ARTIST_ALBUM primary key (ID, ALB_ID)
 );
 
 /*==============================================================*/
 /* Index: ARTIST_ALBUM_PK                                       */
 /*==============================================================*/
 create unique index ARTIST_ALBUM_PK
-  on ARTIST_ALBUM (
-                   ID,
-                   ALB_ID
-    );
+	on ARTIST_ALBUM (
+	                 ID,
+	                 ALB_ID
+		);
 
 /*==============================================================*/
 /* Index: ARTIST_ALBUM_FK                                       */
 /*==============================================================*/
 create index ARTIST_ALBUM_FK
-  on ARTIST_ALBUM (
-                   ID
-    );
+	on ARTIST_ALBUM (
+	                 ID
+		);
 
 /*==============================================================*/
 /* Index: ARTIST_ALBUM2_FK                                      */
 /*==============================================================*/
 create index ARTIST_ALBUM2_FK
-  on ARTIST_ALBUM (
-                   ALB_ID
-    );
+	on ARTIST_ALBUM (
+	                 ALB_ID
+		);
 
 /*==============================================================*/
 /* Table: UPLOAD                                                  */
 /*==============================================================*/
 create table UPLOAD (
-                      ID         SERIAL        not null,
-                      MUS_ID     INT4          not null,
-                      BIN        bytea         null,
-                      IP_ADDRESS VARCHAR(1024) null,
-                      NAME       VARCHAR(1024) not null,
-                      DROPBOX_FILE_ID VARCHAR(1024) null,
-                      DROPBOX_PREV_URL VARCHAR(1024) null,
-                      DROPBOX_FILE_NAME VARCHAR(1024) null,
-                      constraint PK_UPLOAD primary key (ID)
+	                    ID         SERIAL        not null,
+	                    MUS_ID     INT4          not null,
+	                    BIN        bytea         null,
+	                    IP_ADDRESS VARCHAR(1024) null,
+	                    NAME       VARCHAR(1024) not null,
+	                    DROPBOX_FILE_ID VARCHAR(1024) null,
+	                    DROPBOX_PREV_URL VARCHAR(1024) null,
+	                    DROPBOX_FILE_NAME VARCHAR(1024) null,
+	                    constraint PK_UPLOAD primary key (ID)
 );
 
 /*==============================================================*/
 /* Index: UPLOAD_PK                                               */
 /*==============================================================*/
 create unique index UPLOAD_PK
-  on UPLOAD (
-             ID
-    );
+	on UPLOAD (
+	           ID
+		);
 
 /*==============================================================*/
 /* Index: UPLOAD_MUSIC_FK                                         */
 /*==============================================================*/
 create index UPLOAD_MUSIC_FK
-  on UPLOAD (
-             MUS_ID
-    );
+	on UPLOAD (
+	           MUS_ID
+		);
 
 /*==============================================================*/
 /* Table: MUSIC                                                 */
 /*==============================================================*/
 create table MUSIC (
-                     ID     SERIAL      not null,
-                     ALB_ID INT4        not null,
-                     NAME   VARCHAR(32) not null,
-                     UNIQUE (ALB_ID, NAME),
-                     constraint PK_MUSIC primary key (ID)
+	                   ID     SERIAL      not null,
+	                   ALB_ID INT4        not null,
+	                   NAME   VARCHAR(32) not null,
+	                   UNIQUE (ALB_ID, NAME),
+	                   constraint PK_MUSIC primary key (ID)
 );
 
 /*==============================================================*/
 /* Index: MUSIC_PK                                              */
 /*==============================================================*/
 create unique index MUSIC_PK
-  on MUSIC (
-            ID
-    );
+	on MUSIC (
+	          ID
+		);
 
 /*==============================================================*/
 /* Index: ALBUM_MUSIC_FK                                        */
 /*==============================================================*/
 create index ALBUM_MUSIC_FK
-  on MUSIC (
-            ALB_ID
-    );
+	on MUSIC (
+	          ALB_ID
+		);
 
 /*==============================================================*/
 /* Table: NOTIFICATION                                          */
 /*==============================================================*/
 create table NOTIFICATION (
-                            ID      SERIAL        not null,
-                            USE_ID  INT4          not null,
-                            MESSAGE VARCHAR(1024) not null,
-                            constraint PK_NOTIFICATION primary key (ID)
+	                          ID      SERIAL        not null,
+	                          USE_ID  INT4          not null,
+	                          MESSAGE VARCHAR(1024) not null,
+	                          constraint PK_NOTIFICATION primary key (ID)
 );
 
 /*==============================================================*/
 /* Index: NOTIFICATION_PK                                       */
 /*==============================================================*/
 create unique index NOTIFICATION_PK
-  on NOTIFICATION (
-                   ID
-    );
+	on NOTIFICATION (
+	                 ID
+		);
 
 /*==============================================================*/
 /* Index: ACCOUNT_NOTIFICATION_FK                                  */
 /*==============================================================*/
 create index ACCOUNT_NOTIFICATION_FK
-  on NOTIFICATION (
-                   USE_ID
-    );
+	on NOTIFICATION (
+	                 USE_ID
+		);
 
 /*==============================================================*/
 /* Table: review                                               */
 /*==============================================================*/
 create table review (
-                      ID     SERIAL        not null,
-                      ALB_ID INT4          not null,
-                      TEXT   VARCHAR(1024) not null,
-                      SCORE  FLOAT8        not null DEFAULT 0,
-                      constraint PK_review primary key (ID)
+	                    ID     SERIAL        not null,
+	                    ALB_ID INT4          not null,
+	                    TEXT   VARCHAR(1024) not null,
+	                    SCORE  FLOAT8        not null DEFAULT 0,
+	                    constraint PK_review primary key (ID)
 );
 
 /*==============================================================*/
 /* Index: review_PK                                            */
 /*==============================================================*/
 create unique index review_PK
-  on review (
-             ID
-    );
+	on review (
+	           ID
+		);
 
 /*==============================================================*/
 /* Index: ALBUM_REVIEW_FK                                       */
 /*==============================================================*/
 create index ALBUM_REVIEW_FK
-  on review (
-             ALB_ID
-    );
+	on review (
+	           ALB_ID
+		);
 
 /*==============================================================*/
 /* Table: ACCOUNT                                                */
 /*==============================================================*/
 create table ACCOUNT (
-                       ID       SERIAL             not null,
-                       NAME     VARCHAR(32) unique not null,
-                       PASSWORD VARCHAR(32)        not null,
-                       EDITOR   BOOL               not null default false,
-                       DROPBOX_UID VARCHAR(1024) unique null,
-                       DROPBOX_TOKEN VARCHAR(1024) unique null,
-                       DROPBOX_EMAIL VARCHAR(1024) unique null,
-                       constraint PK_ACCOUNT primary key (ID)
+	                     ID       SERIAL             not null,
+	                     NAME     VARCHAR(32) unique not null,
+	                     PASSWORD VARCHAR(32)        not null,
+	                     EDITOR   BOOL               not null default false,
+	                     DROPBOX_UID VARCHAR(1024) unique null,
+	                     DROPBOX_TOKEN VARCHAR(1024) unique null,
+	                     DROPBOX_EMAIL VARCHAR(1024) unique null,
+	                     constraint PK_ACCOUNT primary key (ID)
 );
 
 /*==============================================================*/
 /* Index: ACCOUNT_PK                                               */
 /*==============================================================*/
 create unique index ACCOUNT_PK
-  on ACCOUNT (
-              ID
-    );
+	on ACCOUNT (
+	            ID
+		);
 
 /*==============================================================*/
 /* Table: ACCOUNT_UPLOADS                                            */
 /*==============================================================*/
 create table ACCOUNT_UPLOADS (
-                               ID     INT4 not null,
-                               FIL_ID INT4 not null,
-                               DROPBOX_FILE_ID VARCHAR(1024) unique null,
-                               constraint PK_ACCOUNT_UPLOADS primary key (ID, FIL_ID)
+	                             ID     INT4 not null,
+	                             FIL_ID INT4 not null,
+	                             DROPBOX_FILE_ID VARCHAR(1024) unique null,
+	                             constraint PK_ACCOUNT_UPLOADS primary key (ID, FIL_ID)
 );
 
 /*==============================================================*/
 /* Index: ACCOUNT_UPLOADS_PK                                         */
 /*==============================================================*/
 create unique index ACCOUNT_UPLOADS_PK
-  on ACCOUNT_UPLOADS (
-                      ID,
-                      FIL_ID
-    );
+	on ACCOUNT_UPLOADS (
+	                    ID,
+	                    FIL_ID
+		);
 
 /*==============================================================*/
 /* Index: ACCOUNT_UPLOADS_FK                                         */
 /*==============================================================*/
 create index ACCOUNT_UPLOADS_FK
-  on ACCOUNT_UPLOADS (
-                      ID
-    );
+	on ACCOUNT_UPLOADS (
+	                    ID
+		);
 
 /*==============================================================*/
 /* Index: ACCOUNT_UPLOADS2_FK                                        */
 /*==============================================================*/
 create index ACCOUNT_UPLOADS2_FK
-  on ACCOUNT_UPLOADS (
-                      FIL_ID
-    );
+	on ACCOUNT_UPLOADS (
+	                    FIL_ID
+		);
 
 
 create table ACCOUNT_ALBUM_CHANGES (
-                                     ACCOUNT_ID INT4 not null,
-                                     ALBUM_ID INT4 not null,
-                                     constraint PK_ACCOUNT_ALBUM_CHANGES primary key (ACCOUNT_ID, ALBUM_ID)
+	                                   ACCOUNT_ID INT4 not null,
+	                                   ALBUM_ID INT4 not null,
+	                                   constraint PK_ACCOUNT_ALBUM_CHANGES primary key (ACCOUNT_ID, ALBUM_ID)
 );
 
 alter table ACCOUNT_ALBUM_CHANGES
-  add constraint FK_ACCOUNT_ALBUM_CHANGES_1 foreign key (ACCOUNT_ID)
-    references ACCOUNT (ID)
-    on delete cascade on update restrict;
+	add constraint FK_ACCOUNT_ALBUM_CHANGES_1 foreign key (ACCOUNT_ID)
+		references ACCOUNT (ID)
+			on delete cascade on update restrict;
 
 alter table ACCOUNT_ALBUM_CHANGES
-  add constraint FK_ACCOUNT_ALBUM_CHANGES_2 foreign key (ALBUM_ID)
-    references ALBUM (ID)
-    on delete cascade on update restrict;
+	add constraint FK_ACCOUNT_ALBUM_CHANGES_2 foreign key (ALBUM_ID)
+		references ALBUM (ID)
+			on delete cascade on update restrict;
 
 
 
 create table ACCOUNT_ARTIST_CHANGES (
-                                      ACCOUNT_ID INT4 not null,
-                                      ARTIST_ID INT4 not null,
-                                      constraint PK_ACCOUNT_ARTIST_CHANGES primary key (ACCOUNT_ID, ARTIST_ID)
+	                                    ACCOUNT_ID INT4 not null,
+	                                    ARTIST_ID INT4 not null,
+	                                    constraint PK_ACCOUNT_ARTIST_CHANGES primary key (ACCOUNT_ID, ARTIST_ID)
 );
 
 alter table ACCOUNT_ARTIST_CHANGES
-  add constraint FK_ACCOUNT_ARTIST_CHANGES_1 foreign key (ACCOUNT_ID)
-    references ACCOUNT (ID)
-    on delete cascade on update restrict;
+	add constraint FK_ACCOUNT_ARTIST_CHANGES_1 foreign key (ACCOUNT_ID)
+		references ACCOUNT (ID)
+			on delete cascade on update restrict;
 
 alter table ACCOUNT_ARTIST_CHANGES
-  add constraint FK_ACCOUNT_ARTIST_CHANGES_2 foreign key (ARTIST_ID)
-    references ARTIST (ID)
-    on delete cascade on update restrict;
+	add constraint FK_ACCOUNT_ARTIST_CHANGES_2 foreign key (ARTIST_ID)
+		references ARTIST (ID)
+			on delete cascade on update restrict;
 
 
 alter table ARTIST_ALBUM
-  add constraint FK_ARTIST_A_ARTIST_AL_ARTIST foreign key (ID)
-    references ARTIST (ID)
-    on delete cascade on update restrict;
+	add constraint FK_ARTIST_A_ARTIST_AL_ARTIST foreign key (ID)
+		references ARTIST (ID)
+			on delete cascade on update restrict;
 
 alter table ARTIST_ALBUM
-  add constraint FK_ARTIST_A_ARTIST_AL_ALBUM foreign key (ALB_ID)
-    references ALBUM (ID)
-    on delete cascade on update restrict;
+	add constraint FK_ARTIST_A_ARTIST_AL_ALBUM foreign key (ALB_ID)
+		references ALBUM (ID)
+			on delete cascade on update restrict;
 
 alter table UPLOAD
-  add constraint FK_UPLOAD_UPLOAD_MUSI_MUSIC foreign key (MUS_ID)
-    references MUSIC (ID)
-    on delete cascade on update restrict;
+	add constraint FK_UPLOAD_UPLOAD_MUSI_MUSIC foreign key (MUS_ID)
+		references MUSIC (ID)
+			on delete cascade on update restrict;
 
 alter table MUSIC
-  add constraint FK_MUSIC_ALBUM_MUS_ALBUM foreign key (ALB_ID)
-    references ALBUM (ID)
-    on delete cascade on update restrict;
+	add constraint FK_MUSIC_ALBUM_MUS_ALBUM foreign key (ALB_ID)
+		references ALBUM (ID)
+			on delete cascade on update restrict;
 
 alter table NOTIFICATION
-  add constraint FK_NOTIFICA_ACCOUNT_NOTI_ACCOUNT foreign key (USE_ID)
-    references ACCOUNT (ID)
-    on delete cascade on update restrict;
+	add constraint FK_NOTIFICA_ACCOUNT_NOTI_ACCOUNT foreign key (USE_ID)
+		references ACCOUNT (ID)
+			on delete cascade on update restrict;
 
 alter table review
-  add constraint FK_review_ALBUM_REV_ALBUM foreign key (ALB_ID)
-    references ALBUM (ID)
-    on delete cascade on update restrict;
+	add constraint FK_review_ALBUM_REV_ALBUM foreign key (ALB_ID)
+		references ALBUM (ID)
+			on delete cascade on update restrict;
 
 alter table ACCOUNT_UPLOADS
-  add constraint FK_ACCOUNT_FIL_ACCOUNT_UPLOAD_ACCOUNT foreign key (ID)
-    references ACCOUNT (ID)
-    on delete cascade on update restrict;
+	add constraint FK_ACCOUNT_FIL_ACCOUNT_UPLOAD_ACCOUNT foreign key (ID)
+		references ACCOUNT (ID)
+			on delete cascade on update restrict;
 
 alter table ACCOUNT_UPLOADS
-  add constraint FK_ACCOUNT_FIL_ACCOUNT_UPLOAD_UPLOAD foreign key (FIL_ID)
-    references UPLOAD (ID)
-    on delete cascade on update restrict;
+	add constraint FK_ACCOUNT_FIL_ACCOUNT_UPLOAD_UPLOAD foreign key (FIL_ID)
+		references UPLOAD (ID)
+			on delete cascade on update restrict;
 
 
 DROP FUNCTION IF EXISTS add_album(artist_id SERIAL, name VARCHAR(64), description VARCHAR(1024));
 CREATE FUNCTION add_album(artist_id INT4, asname VARCHAR(64), descriptionz VARCHAR(1024))
-  RETURNS album AS $$
+	RETURNS album AS $$
 DECLARE
-  new_album album%ROWTYPE;
-  t1        text;
-  t2        text;
-  c1        float;
+	new_album album%ROWTYPE;
+	t1        text;
+	t2        text;
+	c1        float;
 BEGIN
-  SELECT * FROM ALBUM A WHERE A.NAME LIKE ('%' || asname || '%')
-    INTO t1, t2, c1;
-  INSERT INTO album (name, description) VALUES (asname, descriptionz) RETURNING *
-    INTO new_album;
-  INSERT INTO artist_album (id, alb_id) VALUES (artist_id, new_album.id);
-  RETURN new_album;
+	SELECT * FROM ALBUM A WHERE A.NAME LIKE ('%' || asname || '%')
+		INTO t1, t2, c1;
+	INSERT INTO album (name, description) VALUES (asname, descriptionz) RETURNING *
+		INTO new_album;
+	INSERT INTO artist_album (id, alb_id) VALUES (artist_id, new_album.id);
+	RETURN new_album;
 END;
 $$
-  LANGUAGE plpgsql STRICT;
+LANGUAGE plpgsql STRICT;
